278c2a64860aa69a072667adb9667ba54f2e5c7b
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-06-22 09:22:11 -0300

    Configurando execução do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,10 @@ import globals from "globals";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    }
+  }
 ];
